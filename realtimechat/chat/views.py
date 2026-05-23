f4f325aff064f3a1ca773830bad0512f2165f3f7
h4tz <blizerd.777@gmail.com>
2026-05-23 15:51:27 +0000

    phase1 -MVP realtimechat

--- realtimechat/chat/views.py
+++ realtimechat/chat/views.py
@@ -1,24 +1,28 @@
-from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
-from .models import Message, PrivateMessage
+from django.shortcuts import render
 
+from .models import Message, PrivateMessage
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'chat/index.html')
-
+    return render(request, "chat/minimal_index.html")
 
 
 def room(request, room_name):
     user = request.user
-    messages = Message.objects.filter(room=room_name).order_by('-timestamp')[:50]
-    return render(request, 'chat/room.html', {'room_name': room_name, 'messages': messages, 'user': user})
-
+    messages = Message.objects.filter(room=room_name).order_by("-timestamp")[:50]
+    return render(
+        request,
+        "chat/minimal_room.html",
+        {"room_name": room_name, "messages": messages, "user": user},
+    )
 
 
 def private_chat(request, username):
     user1 = request.user
     user2 = User.objects.get(username=username)
-    messages = PrivateMessage.objects.filter(user1__in=[user1, user2], user2__in=[user1, user2])
-    return render(request, 'chat/private.html' , { 'user2': user2, 'messages': messages})
+    messages = PrivateMessage.objects.filter(
+        user1__in=[user1, user2], user2__in=[user1, user2]
+    )
+    return render(request, "chat/private.html", {"user2": user2, "messages": messages})
